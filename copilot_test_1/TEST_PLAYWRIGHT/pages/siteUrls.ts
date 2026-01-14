bd2148d393a2bf9b8f3dc6ef0a90948dc123434b
PhuongNguyen9999 <osinsenior@gmail.com>
2026-01-14 14:19:14 +0700

    commit new update

--- copilot_test_1/TEST_PLAYWRIGHT/pages/siteUrls.ts
+++
@@ -1,6 +0,0 @@
-export const SITE_URLS = {
-  BASE_URL: 'https://demoqa.com',
-  LOGIN: 'https://demoqa.com/login',
-  PROFILE: 'https://demoqa.com/profile',
-  BOOKS: 'https://demoqa.com/books'
-};
